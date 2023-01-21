438d08f87c9d7fb2e3b9faf013079e234049054f
Suvojit Chatterjee <91973198+Suvojit2001@users.noreply.github.com>
2023-01-21 19:43:45 +0530

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,10 @@
+"use strict";       //if we dont write var js will not give an error but we want to know our mistake so we write "use strict"
+// to get error
+
+var firstname="suvojit";
+console.log(firstname);
+
+firstname="argha";
+console.log(firstname);
+
+var newname="debu";
---
+++ 23.js
@@ -0,0 +1,31 @@
+//function declaretion 
+
+// function sing(){
+//     console.log("happy birthday to you ");
+// }
+
+// sing();
+
+// function expresion 
+
+const sing = function(){
+    console.log("happy bd to you ");
+}
+sing();
+
+// function sumTwoNumber(number1,number2){
+//     return number1+number2;
+
+// }
+
+//function expression 
+const  sumTwoNumber = function(number1,number2){
+    return number1+number2;
+
+}
+
+const sum= sumTwoNumber(4,5);
+console.log(sum);
+console.log(undefined+undefined);  //NaN -> not a number 
+console.log(2+3+undefined);
+
